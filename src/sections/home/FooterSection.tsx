c9a2f8094220a9ea4d618140c4c04f3c394e0c4b
Refanz <refandasuryasaputra@gmail.com>
2025-02-04 03:49:25 +0700

    fix footer

--- src/sections/home/FooterSection.tsx
+++ src/sections/home/FooterSection.tsx
@@ -2,7 +2,7 @@ import {BiCopyright, BiHeart} from "react-icons/bi";
 
 const FooterSection = () => {
     return (
-        <footer className="flex py-6 gap-1 items-center mx-auto justify-center max-w-[1440px] min-[855px]:px-[80px]">
+        <footer className="flex py-6 text-sm flex-wrap gap-1 items-center justify-center max-w-[1440px] min-[855px]:px-[80px]">
             <BiCopyright/>
             <span>2025 | Designed with </span>
             <BiHeart className="text-red-500"/>
